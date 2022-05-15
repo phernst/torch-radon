d920c3aae6066718cc96192f13171aa363d49d07
Philipp Ernst <philipp.ernst@ovgu.de>
2022-05-15 19:04:34 +0200

    fix contiguous tensors and data type warning

--- torch_radon/differentiable_functions.py
+++ torch_radon/differentiable_functions.py
@@ -8,7 +8,7 @@ class RadonForward(Function):
     @staticmethod
     def forward(ctx, x, angles, tex_cache, vol_cfg, proj_cfg, exec_cfg_generator, exec_cfg=None):
         exec_cfg = exec_cfg_generator(vol_cfg, proj_cfg,  x.dtype == torch.half) if exec_cfg is None else exec_cfg
-        sinogram = cuda_backend.forward(x, angles, tex_cache, vol_cfg, proj_cfg, exec_cfg)
+        sinogram = cuda_backend.forward(x.contiguous(), angles, tex_cache, vol_cfg, proj_cfg, exec_cfg)
         ctx.tex_cache = tex_cache
         ctx.vol_cfg = vol_cfg
         ctx.proj_cfg = proj_cfg.copy()
@@ -21,7 +21,7 @@ class RadonForward(Function):
     def backward(ctx, grad_x):
         angles, = ctx.saved_tensors
         exec_cfg = ctx.exec_cfg_generator(ctx.vol_cfg, ctx.proj_cfg, grad_x.dtype == torch.half)
-        grad = cuda_backend.backward(grad_x, angles, ctx.tex_cache, ctx.vol_cfg, ctx.proj_cfg, exec_cfg)
+        grad = cuda_backend.backward(grad_x.contiguous(), angles, ctx.tex_cache, ctx.vol_cfg, ctx.proj_cfg, exec_cfg)
         return grad, None, None, None, None, None, None
 
 
@@ -29,7 +29,7 @@ class RadonBackprojection(Function):
     @staticmethod
     def forward(ctx, x, angles, tex_cache, vol_cfg, proj_cfg, exec_cfg_generator, exec_cfg=None):
         exec_cfg = exec_cfg_generator(vol_cfg, proj_cfg,  x.dtype == torch.half) if exec_cfg is None else exec_cfg
-        image = cuda_backend.backward(x, angles, tex_cache,  vol_cfg, proj_cfg, exec_cfg)
+        image = cuda_backend.backward(x.contiguous(), angles, tex_cache,  vol_cfg, proj_cfg, exec_cfg)
         ctx.tex_cache = tex_cache
         ctx.vol_cfg = vol_cfg
         ctx.proj_cfg = proj_cfg.copy()
@@ -42,5 +42,5 @@ class RadonBackprojection(Function):
     def backward(ctx, grad_x):
         angles, = ctx.saved_tensors
         exec_cfg = ctx.exec_cfg_generator(ctx.vol_cfg, ctx.proj_cfg, grad_x.dtype == torch.half)
-        grad = cuda_backend.forward(grad_x, angles, ctx.tex_cache, ctx.vol_cfg, ctx.proj_cfg, exec_cfg)
+        grad = cuda_backend.forward(grad_x.contiguous(), angles, ctx.tex_cache, ctx.vol_cfg, ctx.proj_cfg, exec_cfg)
         return grad, None, None, None, None, None, None
--- torch_radon/filtering.py
+++ torch_radon/filtering.py
@@ -52,8 +52,8 @@ class FourierFilters:
         """
         filter_name = filter_name.lower()
 
-        n = np.concatenate((np.arange(1, size / 2 + 1, 2, dtype=np.int),
-                            np.arange(size / 2 - 1, 0, -2, dtype=np.int)))
+        n = np.concatenate((np.arange(1, size / 2 + 1, 2, dtype=int),
+                            np.arange(size / 2 - 1, 0, -2, dtype=int)))
         f = np.zeros(size)
         f[0] = 0.25
         f[1::2] = -1 / (np.pi * n) ** 2
